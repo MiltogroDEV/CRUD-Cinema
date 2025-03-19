de73804eedfd3c28b0790a36ba1049568e273f22
Miltogro <emilton.code@gmail.com>
2025-03-19 14:08:24 -0300

    Remove useless code

--- script/components/apiCall.js
+++ script/components/apiCall.js
@@ -25,30 +25,3 @@ async function apiCall(root, method, data) {
 }
 
 export { apiCall };
-
-
-// async function apiCall(root, method, data){
-//     // const HOST_API = "https://academic-events-api-83ac51d23457.herokuapp.com"
-//     // Host temporária
-//     const HOST_API = "http://127.0.0.1:8080"
-
-//     const requestInfo = {
-//         method: `${method}`,
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify(data)
-//     };
-    
-//     return Promise.resolve(
-//         fetch(`${HOST_API}${root}`, requestInfo)
-//         .then(response => response.json())
-//         .then(data => {
-//             return data;
-//         }).catch(error => {
-//             return error;
-//         })
-//     );
-// }
-
-// export { apiCall };
