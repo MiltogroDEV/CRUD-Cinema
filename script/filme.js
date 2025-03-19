7bb0ec273dd8f27eea224f3447b3e1c21a6f62de
Miltogro <emilton.code@gmail.com>
2025-03-19 14:08:03 -0300

    increment Filme routing

--- script/filme.js
+++ script/filme.js
@@ -11,22 +11,15 @@ async function filmeAtualizar(e){
     const duracao = document.getElementById("/filmes/atualizar/duracao");
     const nome = document.getElementById("/filmes/atualizar/nome");
 
-    const [hour, minute] = duracao.value.split(":").map(Number);
-
     const data = {
-        "sinopse": sipnose.value,
-        "genero": genero.value,
-        "duracao": {
-          "hour": hour,
-          "minute": minute,
-          "second": 0,
-          "nano": 0
-        },
-        "nome": nome.value
-      }
+        "sinopse": `${sipnose.value}`,
+        "genero": `${genero.value}`,
+        "duracao": `${duracao.value}:00`,
+        "nome": `${nome.value}`
+    }
 
     try{
-        attempt = await apiCall(`/filme/atualizar/${id.value}`, "PUT", data);
+        attempt = await apiCall(`/filmes/atualizar/${id.value}`, "PUT", data);
         
         if(attempt.success){
             showMessage("s", `Dados no Console`);
@@ -46,28 +39,89 @@ async function filmeCriar(e){
     let attempt;
     e.preventDefault();
 
-    const id = document.getElementById("/filmes/criar/id");
     const sipnose = document.getElementById("/filmes/criar/sipnose");
     const genero = document.getElementById("/filmes/criar/genero");
     const duracao = document.getElementById("/filmes/criar/duracao");
     const nome = document.getElementById("/filmes/criar/nome");
 
-    const [hour, minute] = duracao.value.split(":").map(Number);
-
     const data = {
-        "sinopse": sipnose.value,
-        "genero": genero.value,
-        "duracao": {
-          "hour": hour,
-          "minute": minute,
-          "second": 0,
-          "nano": 0
-        },
-        "nome": nome.value
-      }
+        "sinopse": `${sipnose.value}`,
+        "genero": `${genero.value}`,
+        "duracao": `${duracao.value}:00`,
+        "nome": `${nome.value}`
+    }
+    
+    try{
+        console.log("Entrou")
+        attempt = await apiCall(`/filmes/criar`, "POST", data);
+        console.log("Saiu")
+        
+        if(attempt.success){
+            showMessage("s", `Dados no Console`);
+            console.log(attempt.success);
+            
+        } else if (attempt.error) {
+            showMessage("e", `Dados no Console`);
+            console.log(attempt.error);
+            
+        }
+    } catch (e){
+        console.log(e);
+    }
+}
+
+async function filmeAchar(e){
+    let attempt;
+    e.preventDefault();
+
+    const id = document.getElementById("/filmes/achar/id");
+    
+    try{
+        attempt = await apiCall(`/filmes/achar/${id.value}`, "POST");
+        
+        if(attempt.success){
+            showMessage("s", `Dados no Console`);
+            console.log(attempt.success);
+            
+        } else if (attempt.error) {
+            showMessage("e", `Dados no Console`);
+            console.log(attempt.error);
+            
+        }
+    } catch (e){
+        console.log(e);
+    }
+}
+
+async function filmeListar(e){
+    let attempt;
+    e.preventDefault();
 
     try{
-        attempt = await apiCall(`/filme/criar`, "POST", data);
+        attempt = await apiCall(`/filmes/listar`, "GET");
+        
+        if(attempt.success){
+            showMessage("s", `Dados no Console`);
+            console.log(attempt.success);
+            
+        } else if (attempt.error) {
+            showMessage("e", `Dados no Console`);
+            console.log(attempt.error);
+            
+        }
+    } catch (e){
+        console.log(e);
+    }
+}
+
+async function filmeDeletar(e){
+    let attempt;
+    e.preventDefault();
+
+    const id = document.getElementById("/filmes/deletar/id");
+    
+    try{
+        attempt = await apiCall(`/filmes/deletar/${id.value}`, "DELETE");
         
         if(attempt.success){
             showMessage("s", `Dados no Console`);
@@ -88,3 +142,12 @@ filmeAtualizarBtn.addEventListener("click", filmeAtualizar);
 
 const filmeCriarBtn = document.getElementById("filmeCriarBtn");
 filmeCriarBtn.addEventListener("click", filmeCriar);
+
+const filmeAcharBtn = document.getElementById("filmeAcharBtn");
+filmeAcharBtn.addEventListener("click", filmeAchar);
+
+const filmeListarBtn = document.getElementById("filmeListarBtn");
+filmeListarBtn.addEventListener("click", filmeListar);
+
+const filmeDeletarBtn = document.getElementById("filmeDeletarBtn");
+filmeDeletarBtn.addEventListener("click", filmeDeletar);
