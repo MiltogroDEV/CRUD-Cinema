0587e5aee33a574c79e75adc119a6f42a0947b99
Miltogro <emilton.code@gmail.com>
2025-03-19 14:08:34 -0300

    Comment useless code

--- script/admin.js
+++ script/admin.js
@@ -1,6 +1,15 @@
 import { apiCall } from "./components/apiCall.js";
 import { showMessage } from "./components/showMessage.js";
 
+// -------- Get
+// const userSession = JSON.parse(localStorage.getItem("userSession"));
+// -------- Delete
+// localStorage.removeItem("userSession");
+// -------- Update
+// userSession.email = clienteEmail.value;
+// userSession.nome = clienteNome.value;
+// localStorage.setItem("userSession", JSON.stringify(userSession));
+
 function mostrarSecao(id) {
     document.querySelectorAll('.main5').forEach(secao => {
         secao.style.display = 'none';
